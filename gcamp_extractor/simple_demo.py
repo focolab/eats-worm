0ea4b2ff2444760264cb9d4d7ff9af88bed2ec58
borchardt <jackson.borchardt@gmail.com>
2020-08-17 23:53:41 -0700

    demo passing eye test but it's a mess

--- gcamp_extractor/simple_demo.py
+++ gcamp_extractor/simple_demo.py
@@ -2,6 +2,7 @@ import napari
 import json
 import numpy as np
 import sys
+from scipy import linalg
 from skimage import io
 from sklearn import mixture
 
@@ -18,6 +19,7 @@ with napari.gui_qt():
     with open("/media/jack/0010-1820/steven_rainbow/dicts/neuron2pixel.json") as f:
         rois = json.load(f)
         neuron = rois["ADAL"]
+        print([(label, len(rois[label]["x"])) for label in rois["neurons"]])
         neuron_pixels = np.zeros(panneuronal_channel.shape)
         neuron_pixels[neuron["z"], neuron["x"], neuron["y"]] = 1
         viewer.add_image(neuron_pixels, name="ADAL", blending='additive', colormap="magenta")
@@ -30,24 +32,53 @@ with napari.gui_qt():
         center = gmm.means_[0].astype(int)
         covariance = gmm.covariances_[0]
         s = 7.815 # 95% chi-square prob from https://people.richland.edu/james/lecture/m170/tbl-chi.html
+
+        A = covariance
+        # calculate eigenvectors and eigenvalues
+        values, vectors = np.linalg.eig(A)
+        # multiply values by sqrt s
+        values = 1. / values
+        # create matrix from eigenvectors
+        Q = vectors
+        # create inverse of eigenvectors matrix
+        R = np.linalg.inv(Q)
+        # create diagonal matrix from eigenvalues
+        L = np.diag(values)
+        # reconstruct the original matrix
+        A = Q.dot(L).dot(R)
+        covariance = A
+
         eigenvalues, eigenvectors = np.linalg.eig(covariance)
-        radii = np.sqrt(s * eigenvalues)
+
+        radii = np.sqrt(1. / eigenvalues)
+        print("eigenvalues: ", eigenvalues)
+        print("eigenvectors: ", eigenvectors)
+        print("radii: ", radii)
 
         # use matrix equation from https://math.stackexchange.com/questions/1403126/what-is-the-general-equation-equation-for-rotated-ellipsoid
-        radial_length_upper_bound = np.max(radii).astype(int) + 1
+        radial_length_upper_bound = np.max(radii).astype(int) + 10
         indices = np.indices(neuron_pixels.shape).transpose(1, 2, 3, 0)
         x_minus_v = indices - center
         rotated = np.zeros(x_minus_v.shape)
         for i in range(center[0] - radial_length_upper_bound, center[0] + radial_length_upper_bound + 1):
             for j in range(center[1] - radial_length_upper_bound, center[1] + radial_length_upper_bound + 1):
                 for k in range(center[2] - radial_length_upper_bound, center[2] + radial_length_upper_bound + 1):
-                    rotated[i, j, k] = np.dot(covariance, x_minus_v[i,j,k])
+                    rotated[i, j, k] = np.dot(covariance, np.array([i, j, k]) - center)
         gmm_ellipsoid = np.zeros(neuron_pixels.shape)
         for i in range(center[0] - radial_length_upper_bound, center[0] + radial_length_upper_bound + 1):
             for j in range(center[1] - radial_length_upper_bound, center[1] + radial_length_upper_bound + 1):
                 for k in range(center[2] - radial_length_upper_bound, center[2] + radial_length_upper_bound + 1):
-                    gmm_ellipsoid[i, j, k] = np.dot(x_minus_v[i,j,k].T, rotated[i,j,k])
-        viewer.add_image( (gmm_ellipsoid > 0).astype(int) * (gmm_ellipsoid <= s ** 3).astype(int), name="gmm ellipsoid", blending='additive', colormap="cyan")
+                    gmm_ellipsoid[i, j, k] = np.dot( (np.array([i, j, k]) - center).T, rotated[i,j,k])
+        
+        # gmm_ellipsoid = gmm_ellipsoid / np.product(eigenvalues)
+
+        inside = 0
+        for observation in X:
+            z, x, y = observation
+            if gmm_ellipsoid[z, x, y] > 0 and gmm_ellipsoid[z, x, y] <= s:
+                inside += 1
+        print("percent covered: ", 100 * inside / len(X))
+        viewer.add_image( (gmm_ellipsoid > 0).astype(int) * (gmm_ellipsoid <= s).astype(int), name="gmm ellipsoid", blending='additive', colormap="cyan")
 
         # region_of_interest = gmm_ellipsoid[center[0] - radial_length_upper_bound: center[0] + radial_length_upper_bound + 1, center[1] - radial_length_upper_bound: center[1] + radial_length_upper_bound + 1, center[2] - radial_length_upper_bound: center[2] + radial_length_upper_bound + 1]
         # print(np.where(region_of_interest <= 60))
@@ -55,3 +86,7 @@ with napari.gui_qt():
         # print(np.min(neuron_pixels), np.max(neuron_pixels))
         # print(np.min(panneuronal_channel), np.max(panneuronal_channel))
         # raise(Exception)
+
+        c_1 = center + 1
+        print(np.dot( c_1.T, np.dot(covariance, c_1 - center) ) )
+        # print(np.dot( c_1.T, np.dot(covariance, c_1 - center) ) / np.product(eigenvalues) )
