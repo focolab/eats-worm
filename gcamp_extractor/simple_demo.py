f360f998af3053f3ccc197aedc90897df9dcdbde
borchardt <jackson.borchardt@gmail.com>
2020-08-20 00:28:08 -0700

    add optional neuron flag

--- gcamp_extractor/simple_demo.py
+++ gcamp_extractor/simple_demo.py
@@ -1,3 +1,4 @@
+import argparse
 import napari
 import json
 import numpy as np
@@ -8,13 +9,15 @@ from sklearn import mixture
 
 np.set_printoptions(threshold=sys.maxsize)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--neuron", nargs='?', default="all")
+args = parser.parse_args()
+
 worm = io.imread("/media/jack/0010-1820/steven_rainbow/20181001-col00-0.tif")
 channels = [worm[:, i, :, : ] for i in range(worm.shape[1])]
+panneuronal_channel = channels[3]
 
 with napari.gui_qt():
-    # viewer = napari.Viewer()
-    panneuronal_channel = channels[3]
-    # viewer.add_image(panneuronal_channel, blending='additive', interpolation='bicubic', colormap='gray', name='panneuronal')
 
     parameters = {}
     stats = {}
@@ -22,11 +25,16 @@ with napari.gui_qt():
     with open("/media/jack/0010-1820/steven_rainbow/dicts/neuron2pixel.json") as f:
         rois = json.load(f)
         neurons = rois["neurons"]
+
+        if args.neuron != "all":
+            neurons = [args.neuron]
+            viewer = napari.Viewer()
+            viewer.add_image(panneuronal_channel, blending='additive', interpolation='bicubic', colormap='gray', name='panneuronal')
+
         for neuron_name in neurons:
             neuron = rois[neuron_name]
             neuron_pixels = np.zeros(panneuronal_channel.shape)
             neuron_pixels[neuron["z"], neuron["x"], neuron["y"]] = 1
-            # viewer.add_image(neuron_pixels, name=neuron_name, blending='additive', colormap="magenta", visible=False)
 
             X = np.transpose(np.vstack((neuron["z"], neuron["x"], neuron["y"])))
             num_components = 1
@@ -71,8 +79,6 @@ with napari.gui_qt():
                     for k in range(center[2] - radial_length_upper_bound, center[2] + radial_length_upper_bound + 1):
                         gmm_ellipsoid[i, j, k] = np.dot( (np.array([i, j, k]) - center).T, rotated[i,j,k])
             
-            # gmm_ellipsoid = gmm_ellipsoid / np.product(eigenvalues)
-
             inside = 0
             for observation in X:
                 z, x, y = observation
@@ -80,22 +86,28 @@ with napari.gui_qt():
                     inside += 1
             print("percent covered: ", 100 * inside / len(X))
             print("precision: ", inside / np.count_nonzero(gmm_ellipsoid) )
-            # viewer.add_image( (gmm_ellipsoid > 0).astype(int) * (gmm_ellipsoid <= s).astype(int), name=neuron_name + " ellipsoid", blending='additive', colormap="cyan", visible=False)
             parameters[neuron_name] = covariance.tolist()
             stats[neuron_name] = {"num_neuron_pixels_covered": inside, "num_neuron_pixels_total": len(X), "num_ellipsoid_pixels": np.count_nonzero(gmm_ellipsoid)}
 
-    with open('simple_demo_parameters.json', 'w') as f:
-        f.write(json.dumps(parameters, indent=4))
-    with open('simple_demo_statistics.json', 'w') as f:
-        f.write(json.dumps(stats, indent=4))
-    
-    total_coverage = sum([stats[neuron]["num_neuron_pixels_covered"] for neuron in stats.keys()]) / sum([stats[neuron]["num_neuron_pixels_total"] for neuron in stats.keys()])
-    total_precision = sum([stats[neuron]["num_neuron_pixels_covered"] for neuron in stats.keys()]) / sum([stats[neuron]["num_ellipsoid_pixels"] for neuron in stats.keys()])
-    coverages = [(neuron, stats[neuron]["num_neuron_pixels_covered"] / stats[neuron]["num_neuron_pixels_total"]) for neuron in stats.keys()]
-    least_covered_neuron, min_coverage = min(coverages, key=lambda x: x[1])
-    most_covered_neuron, max_coverage = max(coverages, key=lambda x: x[1])
+    if args.neuron != "all":
+        viewer.add_image( (gmm_ellipsoid > 0).astype(int) * (gmm_ellipsoid <= s).astype(int), name=neuron_name + " ellipsoid", blending='additive', colormap="cyan", opacity=0.7, visible=True)
+        viewer.add_image(neuron_pixels, name=neuron_name, blending='additive', colormap="magenta", opacity=0.7, visible=True)
 
-    print("total percentage of neuron pixels covered: ", total_coverage)
-    print("total precision across all neurons: ", total_precision)
-    print("highest percentage of neuron pixels covered: ", max_coverage, " for neuron ", most_covered_neuron)
-    print("lowest percentage of neuron pixels covered: ", min_coverage, " for neuron ", least_covered_neuron)
+    else:
+        with open('simple_demo_parameters.json', 'w') as f:
+            f.write(json.dumps(parameters, indent=4))
+        with open('simple_demo_statistics.json', 'w') as f:
+            f.write(json.dumps(stats, indent=4))
+        
+        total_coverage = sum([stats[neuron]["num_neuron_pixels_covered"] for neuron in stats.keys()]) / sum([stats[neuron]["num_neuron_pixels_total"] for neuron in stats.keys()])
+        total_precision = sum([stats[neuron]["num_neuron_pixels_covered"] for neuron in stats.keys()]) / sum([stats[neuron]["num_ellipsoid_pixels"] for neuron in stats.keys()])
+        coverages = [(neuron, stats[neuron]["num_neuron_pixels_covered"] / stats[neuron]["num_neuron_pixels_total"]) for neuron in stats.keys()]
+        least_covered_neuron, min_coverage = min(coverages, key=lambda x: x[1])
+        most_covered_neuron, max_coverage = max(coverages, key=lambda x: x[1])
+
+        print("total percentage of neuron pixels covered: ", total_coverage)
+        print("total precision across all neurons: ", total_precision)
+        print("highest percentage of neuron pixels covered: ", max_coverage, " for neuron ", most_covered_neuron)
+        print("lowest percentage of neuron pixels covered: ", min_coverage, " for neuron ", least_covered_neuron)
+        
+        exit(0)
