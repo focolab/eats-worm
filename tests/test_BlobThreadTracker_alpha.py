9c080ebd427a851dae99eddfd48677bc3c8574b3
gregbubnis <gbubnis@gmail.com>
2021-08-27 10:39:09 -0700

    tinkering modularize extractor

--- tests/test_BlobThreadTracker_alpha.py
+++ tests/test_BlobThreadTracker_alpha.py
@@ -4,12 +4,16 @@
 #   and verifies that they produce identical output
 #
 from gcamp_extractor import Extractor
+from gcamp_extractor.ExtractorBFD import ExtractorBFD
 from gcamp_extractor import Curator
 
 arguments = {
-    'root':'/media/gbubnis/Extreme SSD/yakalo/jackson/2021-05-29/worm5_act1/GCAMP',
-    'numz':12,
-    'frames':[1,2,3,4,5,6,7,8,9,10,11],
+    # 'root':'/media/gbubnis/Extreme SSD/yakalo/jackson/2021-05-29/worm5_act1/GCAMP',
+    # 'numz':12,
+    # 'frames':[1,2,3,4,5,6,7,8,9,10,11],
+    'root':'/home/gbubnis/prj/bfd/FOCO_FCD_v0.1/RLD_selected_recordings/shortened_recs',
+    'numz':10,
+    #'frames':[1,2,3,4,5,6,7,8,9,10,11],
     'offset': 0,
     't':8,
     'gaussian':(41,5,3,1),
@@ -25,15 +29,19 @@ arguments = {
 }
 
 e = Extractor(**arguments)
+ebfd = ExtractorBFD(**arguments)
 
 print('--------')
 e.calc_blob_threads()
-df1 = e.spool.to_dataframe(dims=['Z', 'Y', 'X'])
+e.quantify()
+df1 = e.results_as_dataframe()
 print('--------')
-e.im.reset_t()
-e.calc_blob_threadsv2()
-df2 = e.spool.to_dataframe(dims=['Z', 'Y', 'X'])
+ebfd.calc_blob_threadsv2()
+ebfd.quantify()
+df2 = ebfd.results_as_dataframe()
 print('--------')
+
+
 
 assert all(df1['Z'].values == df2['Z'].values)
 assert all(df1['X'].values == df2['X'].values)
