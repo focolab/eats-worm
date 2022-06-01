d3bef55aef2f81d71408e9d4fc9894544a12605f
crgb0s <ccboswell120@gmail.com>
2022-06-01 08:06:27 -0400

    incorporated all feedback points from PR review

--- eats_worm/Curator.py
+++ eats_worm/Curator.py
@@ -80,11 +80,9 @@ class Curator:
                     self.curate = json.load(f)
                 self.ind = int(self.curate['last'])
                 try:
-                    self.min, self.max = self.curate['contrastMin'], self.curate['contrastMax']
-                    print("Loaded contrast limits from curate.json {} {}".format(self.min, self.max))
+                    self.min, self.max = self.curate['contrast_min'], self.curate['contrast_max']
                 except:
-                    print('Contrast limits not present in curate.json. These will be set to default values.')
-                    self.min, self.max = -1,-1
+                    self.min, self.max = None, None
                 curate_loaded = True
             except:
                 print("No curate.json in output folder. Creating new curation.")
@@ -127,10 +125,9 @@ class Curator:
         if self.tf:
             for c in range(self.tf.numc):
                 self.viewer.add_image(self.tf.get_t(self.t + self.t_offset, channel=c), name='channel {}'.format(c), scale=self.scale, blending='additive', **viewer_settings[self.tf.numc][c])
-            if ((self.min == -1) and (self.max==-1)): #contrast limits not found in curate.json. Set to default
-                self.min, self.max = self.viewer.layers['channel 0'].contrast_limits      
-            else: #set contrast limits to values loaded from curate.json
+            if self.min is not None and self.max is not None:
                 self.viewer.layers['channel 0'].contrast_limits = (self.min, self.max)
+            self.min, self.max = self.viewer.layers['channel 0'].contrast_limits
             self.viewer.layers['channel 0'].events.contrast_limits.connect(lambda:self.update_mm(self.viewer.layers['channel 0'].contrast_limits))
         if self.s:
             self.viewer.add_points(np.empty((0, 3)), symbol='ring', face_color='red', edge_color='red', name='roi', size=2, scale=self.scale)
@@ -411,7 +408,6 @@ class Curator:
     def update_mm(self, contrast_limits):
         self.min = contrast_limits[0]
         self.max = contrast_limits[1]
-        self.curate["contrastMin"], self.curate["contrastMax"] = self.min, self.max
         self.update_figures()
 
     def next(self):
@@ -432,6 +428,7 @@ class Curator:
     def log_curate(self):
         self.do_hacks()
         self.curate['last'] = self.ind
+        self.curate["contrast_min"], self.curate["contrast_max"] = self.min, self.max
         with open(self.path, 'w') as fp:
             json.dump(self.curate, fp)
 
