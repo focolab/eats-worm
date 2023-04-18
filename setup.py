3f825c9449881c3d81058c22bc63451b9e236803
dysprague <dsprague1998@gmail.com>
2023-04-17 22:12:57 -0700

    moved npex to import from git

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
           'opencv-python-headless>=4.1.0.25',
           'matplotlib>=2.1.0',
           'improc @ git+https://github.com/focolab/image-processing',
+          'npex @ git+https://github.com/focolab/npex',
           'imreg_dft',
           'fastcluster',
           'napari[all]',
