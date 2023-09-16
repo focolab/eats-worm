ce088c87295c07000e63d98ad937c2874386f4a8
MunekiIkeda <muneki.ikeda@ucsf.edu>
2023-09-15 17:05:44 -0700

    Update example.py

--- example.py
+++ example.py
@@ -3,11 +3,13 @@
 
 from eats_worm import *
 
-#data_directory = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Immobilized_actual_properScale/green_actual_properScale"; numZ = 157; endT = 10; pixelSize = [0.75, 0.24, 0.20]
+#data_directory = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Immobilized_actual_properScale/green_actual_properScale"; numZ = 157; endT = 2; pixelSize = [0.75, 0.24, 0.20]
 #data_directory = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Moving_actual_properScale/green_actual_properScale"; numZ = 127; endT = 10; pixelSize = [1.42, 0.37, 0.32]
 ## pixelSize = [0.35, 0.75, 0.3545×sin(π×Θ°/180°)]
 #data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230724_OH16290_1_run1"; numZ = 120; endT = 1715; pixelSize = [0.35, 0.75, 0.25]
-data_directory = "E:/SCAPE/Data/tiff_stacks/20230908_GreenBeads4um_run2/20230908_GreenBeads4um_run2"; numZ = 200; endT = 3; pixelSize = [1.8, 1, 1.16]
+#data_directory = "E:/SCAPE/Data/tiff_stacks/20230908_GreenBeads4um_run2/20230908_GreenBeads4um_run2"; numZ = 200; endT = 3; pixelSize = [1.8, 1, 1.16]
+#data_directory = "E:/SCAPE/Data/20230911/tiff_stacks/20230911_GreenBeads4um_run3/Deskewed_-60"; numZ = 200; endT = 3; pixelSize = [0.55, 1, 0.47]
+data_directory = "E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230913/tiff_stacks/20230913_OH16290_3_run1/Deskewed_-45"; numZ = 200; endT = 10; pixelSize = (0.55, 1, 0.47)
 output_directory = data_directory + "/output/"
 
 
@@ -29,9 +31,9 @@ arguments = {
         "window_size": 5, #3
         "min_distance": 11, #1
         "manage_collisions": "prune",
-        "fb_threshold_margin": 50
+        "fb_threshold_margin": 50 #10
     },
-    "register_frames": True,
+    "register_frames": False, #True
     "output_dir": output_directory
 }
 
