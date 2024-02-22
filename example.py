64ca740495642167e69fcf1031af57934728569f
MunekiIkeda <muneki.ikeda@ucsf.edu>
2024-02-22 12:45:04 -0800

    Update example.py

--- example.py
+++ example.py
@@ -3,28 +3,24 @@
 
 from eats_worm import *
 
-#data_directory = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Immobilized_actual_properScale/green_actual_properScale"; numZ = 157; endT = 2; pixelSize = [0.75, 0.24, 0.20]
-#data_directory = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Moving_actual_properScale/green_actual_properScale"; numZ = 127; endT = 10; pixelSize = [1.42, 0.37, 0.32]
-## pixelSize = [0.35, 0.75, 0.3545×sin(π×Θ°/180°)]
-#data_directory = "C:/Users/miked/OneDrive - UCSF/Documents/MATLAB/SCAPE/Data/165mm/NeuroPAL/tiff_stacks/20230724_OH16290_1_run1"; numZ = 120; endT = 1715; pixelSize = [0.35, 0.75, 0.25]
-#data_directory = "E:/SCAPE/Data/tiff_stacks/20230908_GreenBeads4um_run2/20230908_GreenBeads4um_run2"; numZ = 200; endT = 3; pixelSize = [1.8, 1, 1.16]
-#data_directory = "E:/SCAPE/Data/20230911/tiff_stacks/20230911_GreenBeads4um_run3/Deskewed_-60"; numZ = 200; endT = 3; pixelSize = [0.55, 1, 0.47]
-#data_directory = "E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230913/tiff_stacks/20230913_OH16290_3_run1/Deskewed_-45"; numZ = 200; endT = 10; pixelSize = (0.55, 1, 0.47)
-data_directory = "E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230917/tiff_stacks/20230917_OH16290_1_run1/Deskewed_-45"; numZ = 200; endT = 2; pixelSize = (0.55, 1, 0.47)
-output_directory = data_directory + "/output/"
+#data_dir = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Immobilized_actual_properScale/green_actual_properScale"; numz = 157; end_t = 3575; pixel_size = (0.75, 0.24, 0.20)
+#data_dir = "D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Moving_actual_properScale/green_actual_properScale"; numz = 127; end_t = 460; pixel_size = (1.42, 0.37, 0.32)
+#data_dir = "D:/SCAPE/Data/Zyla_4.2PLUS/165mm/NeuroPAL/tiff_stacks/20230716_OH16290_1_run1"; numz = 127; end_t = 1682; pixel_size = (0.35, 0.75, 0.25)
+data_dir = "E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230914/tiff_stacks/20230914_OH16290_3_run1/Deskewed_-45"; numz = 200; end_t = 818; pixel_size = (0.55, 1, 0.39)
+output_dir = data_dir + "/output/"
 
 
 arguments = {
-    "root": data_directory,
-    "numz": numZ,
+    "root": data_dir,
+    "numz": numz,
     #"frames":[1,2,3,4,5,6,7,8,9,10,11],
     "numc": 1,
-    "end_t": endT,
+    "end_t": end_t,
     "offset": 0,
     "gaussian": False,
     "median": 3, #5
     "quantile": 0.963, #0.999
-    "anisotropy": pixelSize,
+    "anisotropy": pixel_size,
     "blob_merge_dist_thresh": 5, #10
     "remove_blobs_dist": 3,
     "algorithm":"tmip_2d_template",
@@ -32,17 +28,16 @@ arguments = {
         "window_size": 5, #3
         "min_distance": 10, #1
         "manage_collisions": "prune",
-        "fb_threshold_margin": 10,
+        "fb_threshold_margin": 50,
     },
     "register_frames": False, #True
-    "output_dir": output_directory
+    "output_dir": output_dir
 }
 
-e = Extractor(**arguments)
+#e = Extractor(**arguments)
+#e.calc_blob_threads()
+#e.quantify(quant_function=background_subtraction_quant_function)
 
-e.calc_blob_threads()
-e.quantify(quant_function=background_subtraction_quant_function)
-
-#e = load_extractor(output_directory)
+e = load_extractor(output_dir)
 
 c = Curator(e=e)
