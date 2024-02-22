4f47f431b3ad4a817f40b82fce921dffd5694615
MunekiIkeda <muneki.ikeda@ucsf.edu>
2024-02-22 12:45:11 -0800

    Update noblob.py

--- noblob.py
+++ noblob.py
@@ -4,10 +4,14 @@
 from eats_worm import *
 
 ## pixelSize = [0.35, 0.75, 0.3545×sin(π×Θ°/180°)]
-#m = MultiFileTiff('D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Moving_actual_properScale/green_actual_properScale', numz=127, numc=1, anisotropy=(1.42, 0.37, 0.32))
-#m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/GreenBeads/20230915/tiff_stacks/20230915_FocalCheck15um_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.55, 1, 0.47))
-#m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230914/tiff_stacks/20230914_OH16290_3_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.55, 0.5, 0.47))
-m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230917/tiff_stacks/20230917_OH16290_1_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.55, 1, 0.47))
+#m = MultiFileTiff('D:/SCAPE/Data/Voleti_et_al_Nat_Meth_2019/Moving_actual_properScale/green_actual_properScale', numz=127, numc=1, anisotropy=(0.32, 0.32, 0.32))
+#m = MultiFileTiff('D:/SCAPE/Data/Zyla/165mm/NeuroPAL/tiff_stacks/20230726_OH16290_1_run2', numz=127, numc=1, anisotropy=(0.25, 0.75, 0.35))
+#m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/GreenBeads/20230911/tiff_stacks/20230911_GreenBeads4um_run3/Deskewed_-60', numz=200, numc=1, anisotropy=(0.39, 1, 0.55))
+#m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/GreenBeads/20230912/tiff_stacks/20230912_GreenBeads4um_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.39, 1, 0.55))
+m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230917/tiff_stacks/20230917_OH16290_3_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.39, 1, 0.55))
+#m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230924/tiff_stacks/20230924_OH16290_5_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.39, 1, 0.55))
+#m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230926/tiff_stacks/20230926_OH16290_1_run1/Deskewed_-45', numz=200, numc=1, anisotropy=(0.39, 0.75, 0.55))
+#m = MultiFileTiff('E:/SCAPE/Data/HiCAM_2000/165mm/NeuroPAL/20230928/tiff_stacks/20230928_OH16290_1_run1/Deskewed_-40', numz=200, numc=1, anisotropy=(0.35, 0.75, 0.55))
 
 viewer = napari.Viewer(ndisplay=3)
 viewer.add_image(m.get_dask_array(), scale=m.anisotropy)
